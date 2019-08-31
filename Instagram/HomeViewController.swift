6f2da143d22e3b051b5ed4fc6de8ed5f21513f63
ohyeah1783 <kawaishinta@gmail.com>
2019-08-31 12:50:15 +0900

    投稿データのクラス作成まで

--- Instagram/HomeViewController.swift
+++ Instagram/HomeViewController.swift
@@ -1,11 +1,3 @@
-//
-//  HomeViewController.swift
-//  Instagram
-//
-//  Created by Shintaro Kawai on 2019/08/30.
-//  Copyright © 2019 shintaro.kawai. All rights reserved.
-//
-
 import UIKit
 
 class HomeViewController: UIViewController {
@@ -13,18 +5,9 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Instagram/PostViewController.swift
+++ Instagram/PostViewController.swift
@@ -1,30 +1,50 @@
-//
-//  PostViewController.swift
-//  Instagram
-//
-//  Created by Shintaro Kawai on 2019/08/30.
-//  Copyright © 2019 shintaro.kawai. All rights reserved.
-//
-
 import UIKit
+import Firebase
+import SVProgressHUD
 
 class PostViewController: UIViewController {
+    var image: UIImage!
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    
+    // 投稿ボタンをタップした時に呼ばれるメソッド
+    @IBAction func handlePostButton(_ sender: Any) {
+        // ImageViewから画像を取得する
+        let imageData = imageView.image!.jpegData(compressionQuality: 0.5)
+        let imageString = imageData!.base64EncodedString(options: .lineLength64Characters)
+        
+        // postDataに必要な情報を取得しておく
+        let time = Date.timeIntervalSinceReferenceDate
+        let name = Auth.auth().currentUser?.displayName
+        
+        // 辞書を作成してFirebaseに保存する
+        let postRef = Database.database().reference().child(Const.PostPath)
+        let postDic = ["caption": textField.text!, "image": imageString, "time": String(time), "name": name!]
+        postRef.childByAutoId().setValue(postDic)
+        
+        // HUDで投稿完了を表示する
+        SVProgressHUD.showSuccess(withStatus: "投稿しました")
+        
+        // 全てのモーダルを閉じて、戦闘画面に戻る
+        UIApplication.shared.keyWindow?.rootViewController?.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    // キャンセルボタンをタップした時に呼ばれるメソッド
+    @IBAction func handleCancelButton(_ sender: Any) {
+        // 画面を閉じる
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // 受け取った画像をimageViewに設定する
+        imageView.image = image
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
