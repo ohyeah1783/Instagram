271faa0eab1a8096c30d5caa585e43a8839a35af
ohyeah1783 <kawaishinta@gmail.com>
2019-08-31 12:58:11 +0900

    投稿データのクラス作成まで

--- Instagram/PostTableViewCell.swift
+++ Instagram/PostTableViewCell.swift
@@ -5,6 +5,7 @@ class PostTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
